1efac417fbbb33347393ea28a4c45b7b2925feeb
iorn <wmt.tkn.121@gmail.com>
2022-06-15 17:31:48 +0900

    app 追加

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import numpy as np
+import cv2
+import os
+from datetime import datetime as dat
 
-test = Flask(__name__)
+app = Flask(__name__, static_url_path='')
+
+# 画像を保存するpathを指定
+IMG_DIR = '/static/images/'
+BASE_DIR = os.path.dirname(__file__)
+IMG_PATH = BASE_DIR+IMG_DIR
 
 
-@test.route('/')
+
+@app.route('/')
 def index():
     return 'hello world'
 
 
 if __name__ == '__main__':
-    test.run()
+    app.run()
